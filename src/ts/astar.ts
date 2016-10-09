ffff3e37d0e037d5272f6b006d3358d0ff0755aa
new4 <new4wang@163.com>
2016-10-09 14:00:21 +0800

    optimize

--- src/ts/astar.ts
+++ src/ts/astar.ts
@@ -2,28 +2,33 @@ import Node from './node';
 import Heap from './heap';
 import { belongTo } from './util';
 
+/**
+ * A* 算法
+ */
 export default class Astar{
   openList: Heap
-  closedList: Node[]
+  closedList: Node[] = []
   startNode: Node
   targetNode: Node
 
-  private b_closedList: belongTo
+  private b_closedList: belongTo = {}
+  private solution: string[] = []
 
   constructor( startNode: Node, targetNode: Node ){
     this.startNode = startNode;
     this.targetNode = targetNode;
-
     this.openList = new Heap( [ startNode ], "F" );
-    this.closedList = [];
-
-    this.b_closedList = {};
   }
 
   // public function
   // ---------------
+
+  /**
+   * 运行 A* 算法
+   */
   run(){
     console.time( "AStar Run !" );
+
     let astar = this;
     while ( !Node.isSame( astar.openList.top(), astar.targetNode ) ){
       let currentNode = astar.openList.pop();
@@ -41,11 +46,11 @@ export default class Astar{
           astar.openList.remove( index );
         }
 
-        if ( astar.isBelongToClosed( nextNode.getValStr() ) && cost < nextNode.getG() ){
+        if ( astar.isBelongToClosed( nextNode ) && cost < nextNode.getG() ){
           console.log( "next 2" );
         }
 
-        if ( index === undefined && !astar.isBelongToClosed( nextNode.getValStr() ) ){
+        if ( index === undefined && !astar.isBelongToClosed( nextNode ) ){
           console.log( "next 3" );
           nextNode.setG( cost );
           nextNode.setF( nextNode.getG() + nextNode.getH( astar.targetNode ) );
@@ -58,25 +63,46 @@ export default class Astar{
     console.log( " astar - ", astar );
 
     let tailNode = astar.openList.top();
-    let p = [];
+    this.solution = [];
     while( tailNode ){
-      p.unshift( tailNode.getValStr() );
+      this.solution.unshift( tailNode.getValStr() );
       tailNode = tailNode.parent;
     }
-    console.log( " p ----- ", p );
 
+    // this.showSolution();
   }
 
+  /**
+   * 获取解决方案数组
+   */
+  getSolution(){
+    return this.solution;
+  }
 
   // private function
   // ---------------
-  private getHeuristicTo(){
 
-  }
-  private isBelongToClosed( str: string ){
+  /**
+   * 判断节点是否在 CLOSED 中
+   */
+  private isBelongToClosed( node: Node ){
+    let str = node.getValStr();
     return !!this.b_closedList[str];
   }
-  private removeFromClosed( str:string ){
 
+  /**
+   * 显示解决方案的具体步骤
+   */
+  private showSolution(){
+    let i = 0,
+        len = this.solution.length,
+        scale = this.targetNode.scale;
+    for ( ; i < len; i ++ ){
+      console.log( `Step ${i} ---` );
+      let item = this.solution[i].split(',');
+      for ( let j = 0; j < scale; j ++ ){
+          console.log( `| ${item[ j*scale ]} ${item[ j*scale + 1 ]} ${item[ j*scale + 2 ]} |` );
+      }
+    }
   }
 }
--- src/ts/node.ts
+++ src/ts/node.ts
@@ -5,34 +5,41 @@ export default class Node{
   zeroIndex: number
   scale: number
   parent: Node
-  F: number
-  G: number
-  fromDir: number
-  toDir: number
+  F: number = 0
+  G: number = 0
   constructor( scale: number, initArr?: number[] ) {
     this.scale = scale;
-    this.value = initArr ? initArr : this.createNodeValueByScale( scale );
+    this.value = initArr ? initArr : this.initNodeValueByScale( scale );
     this.zeroIndex = Math.pow(scale, 2) - 1;
 
     // this.parent = new Node(this.scale);
-    this.F = 0;
-    this.G = 0;
   }
 
   // public function
   // ---------------
 
+  /**
+   * 获取节点的值，将节点的数组表示转换成字符串表示并返回
+   */
   getValStr(){
     return this.value.toString();
   }
 
+  /**
+   * 节点的乱序算法
+   * 随机指定一个方向，令节点向该方向移动，重复上述过程若干次达到乱序的目的
+   */
   shuffle(){
-    for( let i = 0; i < 5; i ++ ){
+    for( let i = 0; i < 5000; i ++ ){
       let direction = Math.floor(Math.random() * 4 + 1);
       this.moveTo( direction );
     }
   }
 
+  /**
+   * 当前节点向方向 direction 移动一次
+   * 其实是节点的数组表示中的数字 0 向方向 direction 移动一次
+   */
   moveTo( direction: number ){
     if ( !this.canMoveTo( direction ) ) return;
     let targetIndex;
@@ -58,6 +65,9 @@ export default class Node{
     this.zeroIndex = targetIndex;
   }
 
+  /**
+   * 获取当前节点在可移动方向上的子节点数组
+   */
   getNextNodes(){
     let node = this;
     let nextNodes: Node[] = [];
@@ -90,35 +100,46 @@ export default class Node{
     }
   }
 
-  getCostToNext(): number{
+  /**
+   * 获取从当前节点走到下一个节点的代价
+   */
+  getCostToNext(){
     return 1;
   }
 
+  /**
+   * 设置节点的 F 值，堆会根据这个值进行排序
+   */
   setF( value: number ){
     this.F = value;
   }
 
+  /**
+   * 获取节点的 G 值
+   */
   getG(){
     return this.G;
   }
 
+  /**
+   * 设置节点的 G 值
+   */
   setG( value: number ){
     this.G = value;
   }
 
+  /**
+   * 获取节点的 H 值
+   */
   getH( targetNode: Node ){
-    let result: number = 0;
-
-    let diff: number = 0;
     let i = 0,
-        len = this.value.length;
+        len = this.value.length,
+        manhatten = 0,
+        diff = 0;
 
     for ( ; i < len; i ++ ){
       if ( this.value[i] !== i + 1 ) diff ++;
-    }
 
-    let manhatten: number = 0;
-    for ( i = 0; i < len; i ++ ){
       let v = this.value[i];
       if( v !== 0 ){
         // now in
@@ -132,14 +153,16 @@ export default class Node{
       }
     }
 
-    result = 5*manhatten + 1*diff;
-
-    return result;
+    return 1*manhatten + 1000*diff;
   }
 
   // private function
   // ----------------
-  private createNodeValueByScale( scale: number ){
+
+  /**
+   * 根据维度 scale 构造节点的初始表示数组
+   */
+  private initNodeValueByScale( scale: number ){
     let val = [];
     for ( let i = 1; i < Math.pow(scale, 2); i ++ ){
       val.push( i );
@@ -149,17 +172,22 @@ export default class Node{
   }
   // static function
   // ---------------
+
+  /**
+   * 判断两个节点是否相等
+   * 通过将节点的数组表示转换成字符串来进行比较
+   */
   static isSame( currentNode: Node, targetNode: Node ){
-    return currentNode.value.toString() === targetNode.value.toString();
+    return currentNode.getValStr() === targetNode.getValStr();
   }
+
+  /**
+   * 基于 node 复制一个新的节点
+   */
   static nodeClone( node: Node ){
     let newNode = new Node( node.scale );
     newNode.value = node.value.slice(0);
     newNode.zeroIndex = node.zeroIndex;
     return newNode;
-  }
-  // cost from one node to another node
-  static costFromN2N( fromNode: Node, toNode: Node ){
-
   }
 }
